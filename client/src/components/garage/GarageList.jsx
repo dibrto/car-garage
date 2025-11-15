b51da7578ebc3b5dc0aec98f84bdf02cea26dba1
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-11-15 16:40:21 +0200

    Add garage items css for all devices

--- client/src/components/garage/GarageList.jsx
+++ client/src/components/garage/GarageList.jsx
@@ -4,11 +4,21 @@ import styles from "./GarageList.module.css"
 export default function GarageList() {
     return (
         <>
-            <div className="absolute inset-0 h-full w-full bg-gray-900/70"></div>
+            <div className="absolute w-full bg-gray-900/70">
+                <div className={styles["garage-grid"]}>
+                    <GarageItem />
+                    <GarageItem />
+                    <GarageItem />
+                    <GarageItem />
+                    <GarageItem />
+                    <GarageItem />
+                    <GarageItem />
+                    <GarageItem />
+                    <GarageItem />
+                    <GarageItem />
+                </div>
+            </div>
 
-            <div className={styles["garage-grid"]}>
-                <GarageItem />
-            </div>            
         </>
     );
 };
