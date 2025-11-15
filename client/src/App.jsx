6dbb072abdbaaaef75fd51d7648192e07fce50f1
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-11-15 12:56:06 +0200

    Add react router to the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
-import Home from "./components/home/Home";
+import { Routes, Route } from "react-router"
+
+import Home from "./components/home/Home"
 
 export default function App() {
 
     return (
-       <Home />
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
     )
 }
