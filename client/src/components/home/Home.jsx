3eef23107016797e754c49b9a7398ee0e80f43d8
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-11-15 14:45:51 +0200

    Add react nav in home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 export default function Home(){
     return (
          <>
@@ -5,7 +7,7 @@ export default function Home(){
                 <div className="container mx-auto flex items-center justify-between">
                     <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-inherit">Car garage</h6>
                     <ul className="ml-10 hidden items-center gap-6 lg:flex">
-                        <li><a href="#" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Home</a></li>
+                        <li><Link to="/" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Home</Link></li>
                         <li><a href="#" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">About Us</a></li>
                         <li><a href="#" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Contact Us</a></li>
                     </ul>
@@ -13,7 +15,7 @@ export default function Home(){
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
                     </svg></span></button>
                 </div>
-                <div className="block w-full basis-full overflow-hidden" style={{height: 0 + "px"}} data-projection-id="3">
+                {/* <div className="block w-full basis-full overflow-hidden" style={{height: 0 + "px"}} data-projection-id="3">
                     <div className="container mx-auto mt-4 rounded-lg bg-white px-6 py-5">
                         <ul className="flex flex-col gap-4 text-blue-gray-900">
                             <li><a href="#" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Home</a></li>
@@ -22,7 +24,7 @@ export default function Home(){
                         </ul>
                         <div className="mt-4 flex gap-2"><button className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20" type="button"><span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><i className="fa-brands fa-facebook text-base"></i></span></button><button className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20" type="button"><span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><i className="fa-brands fa-instagram text-base"></i></span></button><button className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20" type="button"><span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><i className="fa-brands fa-github text-base"></i></span></button></div>
                     </div>
-                </div>
+                </div> */}
             </nav>
             <div className="relative min-h-screen w-full bg-cover bg-no-repeat" style={{backgroundImage: "url('https://bucket.material-tailwind.com/magic-ai/5047c695d8e41617b4b59ad104f0d925a2909e00b1822c20328b992a99c86022.jpg')"}}>
                 <div className="absolute inset-0 h-full w-full bg-gray-900/70"></div>
