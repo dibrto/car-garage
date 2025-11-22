b40c3da2fdfc7996527735f20b7561afe0f68b10
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-11-22 13:46:53 +0200

    Fix home btns

--- client/src/components/header/Navigation.jsx
+++ client/src/components/header/Navigation.jsx
@@ -4,7 +4,7 @@ export default function Navigation(){
     return (
         <>
             <li><Link to="/" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Home</Link></li>
-            <li><Link to="/garages" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Garages</Link></li>
+            <li><Link to="/garages" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Community garages</Link></li>
             <li><Link to="/login" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Login</Link></li>
             <li><Link to="/register" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Register</Link></li>
         </>
