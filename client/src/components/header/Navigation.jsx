4f43dea07f38776554dd17b4717ebccf28cc83df
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-11-28 12:59:50 +0200

    Fix navc to be dynamic

--- client/src/components/header/Navigation.jsx
+++ client/src/components/header/Navigation.jsx
@@ -1,12 +1,25 @@
 import { Link } from "react-router";
+import useUser from "../../hooks/useUser";
 
 export default function Navigation(){
+    const { isAuthenticated } = useUser();
+
     return (
         <>
             <li><Link to="/" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Home</Link></li>
             <li><Link to="/garages" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Community garages</Link></li>
-            <li><Link to="/login" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Login</Link></li>
-            <li><Link to="/register" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Register</Link></li>
+
+            { !isAuthenticated 
+                ? (
+                    <>
+                        <li><Link to="/login" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Login</Link></li>
+                        <li><Link to="/register" className="block antialiased font-sans text-sm leading-normal text-inherit font-medium">Register</Link></li>
+                    </>
+                )
+                : (
+                    <></>
+                )
+            }
         </>
     );
 };
