43926059e1006b7d761aea8f54d2c669a43cf20a
dibr <daniel.bratov@unicontsoft.com>
2025-12-01 16:02:08 +0200

    Create car form

--- client/src/components/garage_car_form/GarageCarForm.jsx
+++ client/src/components/garage_car_form/GarageCarForm.jsx
@@ -14,31 +14,42 @@ export default function GarageCarForm(){
         <div className={styles.page}>
             <div className={styles.card}>
                 <h1 className={styles.title}>Create car</h1>
-                {/* <p className={styles.subtitle}>Personalize</p> */}
+                <p className={styles.subtitle}>Create and add car to your garage</p>
 
                 <form className={styles.form} action={submitHandler}>
                     <label className={styles.label}>
-                        Email
-                        <input
-                            type="email"
-                            className={styles.input}
-                            {...regField("email")}
-                            autoComplete="email"
-                            placeholder="you@example.com"
-                            required
-                        />
+                        Brand
+                        <input type="text" className={styles.input} {...regField("model_make_id")} required />
                     </label>
 
                     <label className={styles.label}>
-                        Password
-                        <input
-                            type="password"
-                            className={styles.input}
-                            {...regField("password")}
-                            autoComplete="current-password"
-                            placeholder="••••••••"
-                            required
-                        />
+                        Model
+                        <input type="text" className={styles.input} {...regField("model_name")} required />
+                    </label>
+
+                    <label className={styles.label}>
+                        Trim
+                        <input type="text" className={styles.input} {...regField("model_trim")} required />
+                    </label>
+
+                    <label className={styles.label}>
+                        Year
+                        <input type="text" className={styles.input} {...regField("model_year")} required />
+                    </label>
+
+                    <label className={styles.label}>
+                        Horse power
+                        <input type="text" className={styles.input} {...regField("model_engine_power_ps")} required />
+                    </label>
+
+                    <label className={styles.label}>
+                        Engine fuel
+                        <input type="text" className={styles.input} {...regField("model_engine_fuel")} required />
+                    </label>
+
+                    <label className={styles.label}>
+                        Image URL
+                        <input type="text" className={styles.input} {...regField("model_imageUrl")} required />
                     </label>
                    
                     <button type="submit" className={styles.submit}>Create</button>
