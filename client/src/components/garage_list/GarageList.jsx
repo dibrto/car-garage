b6b0e879079211608164e4fa46e09845ef07739a
dibr <daniel.bratov@unicontsoft.com>
2025-12-02 17:46:59 +0200

    Filter community garages to exclude logged user garage

--- client/src/components/garage_list/GarageList.jsx
+++ client/src/components/garage_list/GarageList.jsx
@@ -1,13 +1,18 @@
 import styles from "./GarageList.module.css"
 import GarageItem from "./GarageItem";
 import useFetch from "../../hooks/useFetch";
+import useUser from "../../hooks/useUser";
 
 export default function GarageList() {
     const { data: garages } = useFetch("data", "garages?load=author%3D_ownerId%3Ausers", []);
-    
+    const { user } = useUser();
+
+
+    const filteredGarages = user ? garages.filter(g => g._ownerId !== user._id) : garages;
+
     return (
         <div className={styles["garage-grid"]}>
-            {garages.map(garage => <GarageItem key={garage._id} garageData={garage}/> )}
+            {filteredGarages.map(garage => <GarageItem key={garage._id} garageData={garage}/> )}
         </div>
     );
 };
