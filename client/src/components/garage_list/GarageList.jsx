befc16cd48c7b3937458cb0b9e053f075fe6c4ce
dibr <daniel.bratov@unicontsoft.com>
2025-12-02 17:31:00 +0200

    Remove render filter in community garages

--- client/src/components/garage_list/GarageList.jsx
+++ client/src/components/garage_list/GarageList.jsx
@@ -1,15 +1,13 @@
 import styles from "./GarageList.module.css"
 import GarageItem from "./GarageItem";
 import useFetch from "../../hooks/useFetch";
-import useUser from "../../hooks/useUser";
 
 export default function GarageList() {
     const { data: garages } = useFetch("data", "garages?load=author%3D_ownerId%3Ausers", []);
-    const { user } = useUser();
     
     return (
         <div className={styles["garage-grid"]}>
-            {garages.map(garage => user._id !== garage._ownerId && <GarageItem key={garage._id} garageData={garage}/> )}
+            {garages.map(garage => <GarageItem key={garage._id} garageData={garage}/> )}
         </div>
     );
 };
