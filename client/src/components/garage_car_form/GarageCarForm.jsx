2540899dba20911ea6d84febdbbd030a30060501
dibr <daniel.bratov@unicontsoft.com>
2025-12-04 16:23:50 +0200

    Add year dropdown

--- client/src/components/garage_car_form/GarageCarForm.jsx
+++ client/src/components/garage_car_form/GarageCarForm.jsx
@@ -5,27 +5,16 @@ import useForm from "../../hooks/useForm";
 import styles from "./GarageCarForm.module.css"
 
 const initVals = {
-    // model_make_id : ""
-    model_imageUrl : ""
-    , model_year : ""
-    , model_name : ""
-    , model_trim : ""
-    , make_display : ""
-    , make_country : ""
-    // , model_engine_power_ps : ""
-    // , model_engine_fuel : ""
+    year: ""
 };
 
 // TODO: make request to car query api
 export default function GarageCarForm(){
-    const { data: years } = useFetch("carQuery", "years", {});
+    const { data: years, } = useFetch("carQuery", "years", { Years: {} });
     const { garageId, carId } = useParams();
     const {data, setData, regField} = useForm(initVals);
     const { fetchData } = useFetch();
-    const navigate = useNavigate();
-
-    console.log(years);
-    
+    const navigate = useNavigate();    
 
     // // edit car 
     // useEffect(() => {
@@ -59,36 +48,37 @@ export default function GarageCarForm(){
         navigate(`/garages/${garageId}`);
     };
 
+    const submitHandler = () => {
+        console.log(data);
+        
+    };
+
+    let yearsOptions = [];
+    const minYear = Number(years.Years.min_year);
+    const maxYear = Number(years.Years.max_year);
+    for (let i = maxYear; i >= minYear; i--) {
+        yearsOptions.push(i);
+    }
+                            
     return (
-          <div className="w-full max-w-4xl mx-auto mt-30 p-6 
-                        rounded-2xl bg-black/30 backdrop-blur-lg 
-                        border border-white/10 shadow-xl">
-
-            <h2 className="text-center text-2xl font-semibold text-white mb-6">
-                Select a car
-            </h2>
-
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-
+        <div className="w-full max-w-4xl mx-auto mt-30 p-6 rounded-2xl bg-black/30 backdrop-blur-lg border border-white/10 shadow-xl">
+            <h2 className="text-center text-2xl font-semibold text-white mb-6">Select a car</h2>
+            <form action={submitHandler} className="grid grid-cols-1 md:grid-cols-4 gap-6">
                 {/* Year */}
                 <div>
-                    <label className="block text-white/80 mb-1">Year</label>
+                    <div className="block text-white/80 mb-1">Year</div>
                     <select
-                        // value={year}
-                        // onChange={(e) => setYear(e.target.value)}
-                        className="w-full p-3 bg-white/10 text-white rounded-xl 
-                                   border border-white/10 focus:outline-none 
-                                   focus:ring-2 focus:ring-blue-500">
+                        className="w-full p-3 bg-white/10 rounded-xl border border-white/10 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        {...regField("year")}
+                    >
                         <option value="">---</option>
-                        <option>2024</option>
-                        <option>2023</option>
-                        <option>2022</option>
-                    </select>
+                        { yearsOptions.map(year => <option key={year} value={year}>{year}</option>) }                        
+                    </select> 
                 </div>
 
                 {/* Make */}
                 <div>
-                    <label className="block text-white/80 mb-1">Make</label>
+                    <div className="block text-white/80 mb-1">Make</div>
                     <select
                         // value={make}
                         // onChange={(e) => setMake(e.target.value)}
@@ -104,7 +94,7 @@ export default function GarageCarForm(){
 
                 {/* Model */}
                 <div>
-                    <label className="block text-white/80 mb-1">Model</label>
+                    <div className="block text-white/80 mb-1">Model</div>
                     <select
                         // value={model}
                         // onChange={(e) => setModel(e.target.value)}
@@ -119,7 +109,7 @@ export default function GarageCarForm(){
 
                 {/* Trim */}
                 <div>
-                    <label className="block text-white/80 mb-1">Trim</label>
+                    <div className="block text-white/80 mb-1">Trim</div>
                     <select
                         // value={trim}
                         // onChange={(e) => setTrim(e.target.value)}
@@ -132,7 +122,10 @@ export default function GarageCarForm(){
                         <option>Premium</option>
                     </select>
                 </div>
-            </div>                                
+
+                <button className="bg-amber-50">Submit</button>
+            </form>
+
         </div>
     );
 };
